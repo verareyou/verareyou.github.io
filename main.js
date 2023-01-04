7465b3f8e652efdfce3048fa954202666ae6071e
himanshu <hmxnsu.blvck@gmail.com>
2023-01-04 14:59:34 +0530

    fixed some issues

--- main.js
+++ main.js
@@ -2,23 +2,37 @@
 const primary = document.getElementById('header');
 const body = document.body;
 const modebtn = document.getElementById('modebtn');
+const fuck = document.getElementById('fuck');
+
 
 const mode = (color) => {
   primary.style.background = `${color}`
+  if(color == "#111111"){
+    fuck.style.background = "white"
+  }
+  else{
+    fuck.style.background = "#111111"
+  }
 }
 
 const fontmode = (color) => {
   primary.style.color = `${color}`
+  if(color == "#111111"){
+    fuck.style.color = "white"
+  }
+  else{
+    fuck.style.color = "#111111"
+  }
 }
 
 modebtn.addEventListener("click",() => {
-  if(primary.style.background == "#111111"){
-    mode("white")
-    fontmode("#111111")
+  if(primary.style.background == "white"){
+    mode("#111111")
+    fontmode("white")
   }
   else{
-    mode("#111111")
-    fontmode("white");
+    mode("white")
+    fontmode("#111111")
 
   }
 })
