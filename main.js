a7c783f6ef07930386ff3b6577a6da3700f8d8d0
himanshu <hmxnsu.blvck@gmail.com>
2023-01-04 14:27:22 +0530

    adding dark mode

---
+++ main.js
@@ -0,0 +1,24 @@
+
+const primary = document.getElementById('header');
+const body = document.body;
+const modebtn = document.getElementById('modebtn');
+
+const mode = (color) => {
+  primary.style.background = `${color}`
+}
+
+const fontmode = (color) => {
+  primary.style.color = `${color}`
+}
+
+modebtn.addEventListener("click",() => {
+  if(primary.style.background == "white"){
+    mode("#111111")
+    fontmode("white")
+  }
+  else{
+    mode("white")
+    fontmode("#111111")
+
+  }
+})
