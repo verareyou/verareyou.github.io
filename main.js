3434d18daea5dc7b72f5ea78e3503cce55e76137
himanshu <hmxnsu.blvck@gmail.com>
2023-01-04 18:56:36 +0530

    j

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ const body = document.body;
 const modebtn = document.getElementById('modebtn');
 const modebtnspan = document.getElementById('modebtnspan');
 const fuck = document.getElementById('fuck');
+const fluidbox = document.getElementById('fluidbox');
 
 
 const mode = (color) => {
   primary.style.background = `${color}`
+  fluidbox.style.background = `${color}`
   if(color == "#111111"){
     fuck.style.background = "white"
   }
@@ -30,12 +32,12 @@ modebtn.addEventListener("click",() => {
   if(primary.style.background == "white"){
     mode("#111111")
     fontmode("white")
-    modebtnspan.innerText = "Dark."
+    modebtnspan.innerText = "dark"
   }
   else{
     mode("white")
     fontmode("#111111")
-    modebtnspan.innerText = "Light."
+    modebtnspan.innerText = "light"
 
   }
 
